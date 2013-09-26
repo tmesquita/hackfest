495d1d03dec8502da4fc640e97414705992d14c8
Thomas Mesquita <thomas@doubleencore.com>
2013-09-25 21:10:31 -0600

    working code

--- simon.rb
+++ simon.rb
@@ -1,40 +1,41 @@
 require "arduino_firmata"
 
-arduino = ArduinoFirmata.connect
-puts "firmata version #{arduino.version}"
+@arduino = ArduinoFirmata.connect
+puts "firmata version #{@arduino.version}"
 
+@random = Random.new
+
+colors = {red: 12, green: 11, blue: 10, yellow: 9}
 letters = %w(a s w d)
 letter_colors = {'a' => colors[:red], 's' => colors[:green], 'w' => colors[:blue], 'd' => colors[:yellow]}
 sequence = []
 did_win = true
 
-colors = {red: 12, green: 11, blue: 10, yellow: 9}
 
 sleep 0.5
-arduino.digital_write 12, true
-arduino.digital_write 11, true
-arduino.digital_write 10, true
-arduino.digital_write 9, true
+@arduino.digital_write 12, true
+@arduino.digital_write 11, true
+@arduino.digital_write 10, true
+@arduino.digital_write 9, true
 sleep 1.5
-arduino.digital_write 12, false
-arduino.digital_write 11, false
-arduino.digital_write 10, false
-arduino.digital_write 9, false
+@arduino.digital_write 12, false
+@arduino.digital_write 11, false
+@arduino.digital_write 10, false
+@arduino.digital_write 9, false
 
-
+def blink_color(color)
+	@arduino.digital_write color, true
+	sleep 0.5
+	@arduino.digital_write color, false
+end
 
 while did_win
-	sequence << letters.sample
+	sequence << letters[@random.rand(100) % 4]
 	begin
 		puts "CURRENT SEQUENCE = #{sequence}"
 		sequence.each do |e|
 			sleep 1
 			blink_color(letter_colors[e])
-			puts "wat #{letter_colors[e.to_sym]}"
-			arduino.digital_write letter_colors[e], true
-  		sleep 0.5
-  		arduino.digital_write letter_colors[e], false
-  		sleep 0.5
 		end
 		system("stty raw -echo")
 		input = []
@@ -50,16 +51,16 @@ while did_win
 	if input == sequence
 		puts "WIN"
 	else
-		(0..5).each do i
-			arduino.digital_write 12, true
-			arduino.digital_write 11, true
-			arduino.digital_write 10, true
-			arduino.digital_write 9, true
+		(0..5).each do |i|
+			@arduino.digital_write 12, true
+			@arduino.digital_write 11, true
+			@arduino.digital_write 10, true
+			@arduino.digital_write 9, true
 			sleep 0.5
-			arduino.digital_write 12, false
-			arduino.digital_write 11, false
-			arduino.digital_write 10, false
-			arduino.digital_write 9, false
+			@arduino.digital_write 12, false
+			@arduino.digital_write 11, false
+			@arduino.digital_write 10, false
+			@arduino.digital_write 9, false
 			sleep 0.5
 		end
 		puts "Try again"
@@ -67,8 +68,4 @@ while did_win
 	end
 end
 
-def blink_color(color)
-	arduino.digital_write letter_colors[e], true
-	sleep 0.5
-	arduino.digital_write letter_colors[e], false
-end
+
