bd5a1028e937bbad7842546b4fc25550e62669fa
Thomas Mesquita <thomas@doubleencore.com>
2013-09-25 20:52:20 -0600

    blinking colors to show sequence

--- simon.rb
+++ simon.rb
@@ -1,16 +1,46 @@
+require "arduino_firmata"
+
+arduino = ArduinoFirmata.connect
+puts "firmata version #{arduino.version}"
+
 letters = %w(a s w d)
+letter_colors = {'a' => colors[:red], 's' => colors[:green], 'w' => colors[:blue], 'd' => colors[:yellow]}
 sequence = []
 did_win = true
+
+colors = {red: 12, green: 11, blue: 10, yellow: 9}
+
+sleep 0.5
+arduino.digital_write 12, true
+arduino.digital_write 11, true
+arduino.digital_write 10, true
+arduino.digital_write 9, true
+sleep 1.5
+arduino.digital_write 12, false
+arduino.digital_write 11, false
+arduino.digital_write 10, false
+arduino.digital_write 9, false
+
+
 
 while did_win
 	sequence << letters.sample
 	begin
-		system("stty raw -echo")
 		puts "CURRENT SEQUENCE = #{sequence}"
+		sequence.each do |e|
+			sleep 1
+			blink_color(letter_colors[e])
+			puts "wat #{letter_colors[e.to_sym]}"
+			arduino.digital_write letter_colors[e], true
+  		sleep 0.5
+  		arduino.digital_write letter_colors[e], false
+  		sleep 0.5
+		end
+		system("stty raw -echo")
 		input = []
 		while str = STDIN.getc
 			input << str
-			puts "INPUT: #{strd}\n"
+			puts "INPUT: #{str}\n"
 			break if input.size == sequence.size
 		end
 	ensure
@@ -20,7 +50,25 @@ while did_win
 	if input == sequence
 		puts "WIN"
 	else
+		(0..5).each do i
+			arduino.digital_write 12, true
+			arduino.digital_write 11, true
+			arduino.digital_write 10, true
+			arduino.digital_write 9, true
+			sleep 0.5
+			arduino.digital_write 12, false
+			arduino.digital_write 11, false
+			arduino.digital_write 10, false
+			arduino.digital_write 9, false
+			sleep 0.5
+		end
 		puts "Try again"
 		did_win = false
 	end
+end
+
+def blink_color(color)
+	arduino.digital_write letter_colors[e], true
+	sleep 0.5
+	arduino.digital_write letter_colors[e], false
 end
